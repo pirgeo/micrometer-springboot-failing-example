f88ac420a6921fd6604af689135d218a34597b92
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2022-08-09 13:40:03 +0200

    polish

--- src/main/java/org/example/MyRestController.java
+++ src/main/java/org/example/MyRestController.java
@@ -1,13 +1,13 @@
 package org.example;
 
-//import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @org.springframework.web.bind.annotation.RestController
 public class MyRestController {
     public MyRestController() {
     }
 
-    // I first thought that this causes the error, but even if this is not here the error persists
-    //    public MyRestController(MeterRegistry registry) {
-    //    }
+    public MyRestController(MeterRegistry registry) {
+        // I first thought that this causes the error, but even if this is not here the error persists
+    }
 }
